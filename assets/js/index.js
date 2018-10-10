953f636dd7463584de5c7b1030d179a905c4b4cf
lambret <aurelie.l-02@hotmail.fr>
2018-10-10 11:38:29 +0200

    exercice 4

---
+++ assets/js/index.js
@@ -0,0 +1,12 @@
+function verification(){
+  var password = document.getElementById('password').value;
+  var confirmPassword = document.getElementById('confirmPassword').value;
+  if(password != confirmPassword){
+    document.getElementById('password').style.border='3px solid red';
+    document.getElementById('confirmPassword').style.border='3px solid red';
+}
+else if(password == confirmPassword){
+  document.getElementById('password').style.border='3px solid green';
+    document.getElementById('confirmPassword').style.border='3px solid green';
+}
+}
